8b087617cfb78060c21fe3a97226492d78799e02
eimanhakimy <33915454+eimanhakimy@users.noreply.github.com>
2018-01-21 12:53:47 +0800

    done

--- Lab-Exercise-4.cpp
+++ Lab-Exercise-4.cpp
@@ -4,21 +4,19 @@
 
 #include <iostream>
 
+using namespace std;
 
 int main()
 {
-	cout<<"Display the pattern"<<endl;
-	cout<<"-------------------"<<endl;
-
-	i,j,rows;
-	cout<<"Input number of rows :"<<endl;
-
-
-	for(i=1;i<=rows;i++)
-	{
+   int i,j,rows;
+    cout << "\n\n Display the pattern using number repeating for a row:\n";
+    cout << "----------------------------------------------------------\n";
+    cout << " Input number of rows: ";
+    cin >> rows;
+   for(i=1;i<=rows;i++)
+   {
 	for(j=1;j<=i;j++)
-		
+	   cout<<i;
 	cout<<endl;
-	}
+   }	
 }
-	
